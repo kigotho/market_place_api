387b004c0e87ae8909c3e34cb068ccd88b71df5c
kigotho <mkigotho2@gmail.com>
2016-07-23 13:14:07 +0300

    users destroy and update actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,10 +9,22 @@ class Api::V1::UsersController < ApplicationController
         render json: { errors: user.errors }, status: 422
       end
     end
+  def update
+        user = User.find(params[:id])
+
+        if user.update(user_params)
+          render json: user, status: 200, location: [:api, user]
+        else
+          render json: { errors: user.errors }, status: 422
+        end
+  end
   def show
     respond_with User.find(params[:id])
   end
-
+  def destroy
+    user = User.find(params[:id])
+      user.destroy
+      head 204  end
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ MarketPlaceApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-        resources :users, :only => [:show, :create]
+        resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
